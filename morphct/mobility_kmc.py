73cbcbb0b9b6ce2d8ad6dfd3e9d853757e189d90
Jenny Fothergill <jennyfothergill@u.boisestate.edu>
2021-02-22 17:16:29 -0700

    update get_jobslist

--- morphct/mobility_kmc.py
+++ morphct/mobility_kmc.py
@@ -331,8 +331,6 @@ def run_single_kmc(
         jobs_to_run,
         KMC_directory,
         AA_morphdict,
-        CG_morphdict,
-        CGtoAAID_list,
         param_dict,
         chromo_list,
         CPU_rank,
@@ -529,47 +527,27 @@ def run_single_kmc(
     send_end.send(save_data)
 
 
-def get_jobslist(
-    AA_morphology_dict,
-    CG_morphology_dict,
-    CG_to_AAID_master,
-    param_dict,
-    chromophore_list,
-):
+def get_jobslist(sim_times, n_holes=0, n_elec=0, nprocs=None, seed=None):
     # Get the random seed now for all the child processes
-    if param_dict["random_seed_override"] is not None:
-        np.random.seed(param_dict["random_seed_override"])
-    try:
-        if param_dict["use_average_hop_rates"]:
-            print("Be advised: use_average_hop_rates is set to {}".format(
-                        repr(param_dict["use_average_hop_rates"])))
-            print("Average Intra-molecular hop rate: {}".format(
-                param_dict["average_intra_hop_rate"]))
-            print("Average Inter-molecular hop rate: {}".format(
-                param_dict["average_inter_hop_rate"]))
-    except KeyError:
-        pass
+    if seed is not None:
+        np.random.seed(seed)
+    if nprocs is None:
+        nprocs = mp.cpu_count()
     # Determine the maximum simulation times based on the parameter dictionary
-    simulation_times = param_dict["simulation_times"]
-    carrier_list = []
+    carriers = []
     # Modification: Rather than being clever here with the carriers, I'm just
     # going to create the master list of jobs that need running and then
     # randomly shuffle it. This will hopefully permit a similar number of holes
     # and electrons and lifetimes to be run simultaneously providing adequate
     # statistics more quickly
-    for lifetime in simulation_times:
-        for carrier_no in range(param_dict["number_of_holes_per_simulation_time"]):
-            carrier_list.append([carrier_no, lifetime, "hole"])
-        for carrier_no in range(
-            param_dict["number_of_electrons_per_simulation_time"]
-        ):
-            carrier_list.append([carrier_no, lifetime, "electron"])
-    np.random.shuffle(carrier_list)
-    proc_IDs = param_dict["proc_IDs"]
-    step = math.ceil(len(carrier_list) / len(proc_IDs))
-    jobs_list = [
-        carrier_list[i : i + step] for i in range(0, len(carrier_list), step)
-    ]
+    for lifetime in sim_times:
+        for carrier_no in range(n_holes):
+            carriers.append([carrier_no, lifetime, "hole"])
+        for carrier_no in range(n_elec):
+            carriers.append([carrier_no, lifetime, "electron"])
+    np.random.shuffle(carriers)
+    step = math.ceil(len(carriers) / nprocs)
+    jobs_list = [carriers[i : i+step] for i in range(0, len(carriers), step)]
     return jobs_list
 
 
@@ -577,8 +555,6 @@ def run_kmc(
         jobs_list,
         KMC_directory,
         AA_morphdict,
-        CG_morphdict,
-        CGtoAAID_list,
         param_dict,
         chromo_list
         ):
@@ -593,8 +569,6 @@ def run_kmc(
             jobs,
             KMC_directory,
             AA_morphdict,
-            CG_morphdict,
-            CGtoAAID_list,
             param_dict,
             chromo_list,
             proc_ID,
