4b2f665b4b441354777b4a90285a9d5d133526c7
Jenny Fothergill <jennyfothergill@u.boisestate.edu>
2021-02-23 18:32:11 -0700

    updating functions through run single kmc

--- morphct/mobility_kmc.py
+++ morphct/mobility_kmc.py
@@ -23,9 +23,10 @@ class Carrier:
         carrier_no,
         box,
         temp,
-        hop_limit = None,
+        n,
+        hop_limit=None,
         record_history=True,
-        mol_id_dict = None,
+        mol_id_dict=None,
         use_average_hop_rates = False,
         average_intra_hop_rate = None,
         average_inter_hop_rate = None,
@@ -38,7 +39,7 @@ class Carrier:
         self.image = np.array([0, 0, 0])
         self.initial_chromo = chromo
         self.current_chromo = chromo
-        self.lambda_ij = self.current_chromo.reorganisation_energy
+        self.lambda_ij = self.current_chromo.reorganization_energy
         self.hop_limit = hop_limit
         self.temp = temp
         self.lifetime = lifetime
@@ -46,7 +47,6 @@ class Carrier:
 
         self.hole_history = None
         self.electron_history = None
-        n = len(chromo_list)
         if self.current_chromo.species == "donor":
             self.c_type = "hole"
             if record_history:
@@ -58,7 +58,7 @@ class Carrier:
 
         self.n_hops = 0
         self.box = box
-        self.displacement = None
+        self.displacement = 0
         self.mol_id_dict = mol_id_dict
 
         self.use_average_hop_rates = use_average_hop_rates
@@ -77,20 +77,20 @@ class Carrier:
 
         self.hopping_prefactor = hopping_prefactor
 
-    def calculate_displacement(self):
-        init_pos = self.initial_chromo.pos
-        final_pos = self.current_chromo.pos
+    def update_displacement(self):
+        init_pos = self.initial_chromo.center
+        final_pos = self.current_chromo.center
         displacement = final_pos - init_pos + self.image * self.box
-        return np.linalg.norm(displacement)
+        self.displacement = np.linalg.norm(displacement)
 
-    def calculate_hop(self, chromo_list):
+    def calculate_hop(self, chromo_list, verbose=False):
         # Terminate if the next hop would be more than the termination limit
         if self.hop_limit is not None:
             if self.n_hops + 1 > self.hop_limit:
-                return 1
+                return False
         # Determine the hop times to all possible neighbors
         hop_times = []
-        if self.use_average_hop_rates is True:
+        if self.use_average_hop_rates:
             # Use the average hop values given in the parameter dict to pick a
             # hop
             for i,img in self.current_chromo.neighbors:
@@ -110,7 +110,7 @@ class Carrier:
             # Obtain the reorganisation energy in J (from eV)
             for i_neighbor, ti in enumerate(self.current_chromo.neighbors_ti):
                 # Ignore any hops with a NoneType transfer integral
-                if transfer_integral is None:
+                if ti is None:
                     continue
                 # index of the neighbor in the main list
                 n_ind = self.current_chromo.neighbors[i_neighbor][0]
@@ -119,15 +119,16 @@ class Carrier:
                 prefactor = self.hopping_prefactor
                 # Get the relative image so we can update the carrier image
                 # after the hop
-                rel_image = self.current_chromo.neighbors[i_neighbor][1]
+                rel_img = self.current_chromo.neighbors[i_neighbor][1]
                 # All of the energies are in eV currently, so convert them to J
                 if self.use_VRH is True:
                     neighbor_chromo = chromo_list[n_ind]
-                    neighbor_pos = neighbor_chromo.pos + rel_image * self.box
+                    neighbor_pos = neighbor_chromo.center + rel_img * self.box
 
                     # Chromophore separation needs converting to m
-                    sep = np.linalg.norm(self.current_chromo.pos - neighbor_pos)
-                    sep *= 1e-10
+                    sep = np.linalg.norm(
+                            self.current_chromo.center - neighbor_pos
+                            ) * 1e-10
 
                     hop_rate = hf.calculate_carrier_hop_rate(
                         self.lambda_ij,
@@ -151,7 +152,7 @@ class Carrier:
                     )
                 hop_time = hf.determine_event_tau(hop_rate)
                 # Keep track of the chromophoreid and the corresponding tau
-                hop_times.append([n_ind, hop_time, rel_image])
+                hop_times.append([n_ind, hop_time, rel_img])
         # Sort by ascending hop time
         hop_times.sort(key=lambda x: x[1])
         if len(hop_times) == 0:
@@ -167,8 +168,12 @@ class Carrier:
         # Move the carrier and send the contiuation signal to
         # singleCoreRunKMC.py
         # Take the quickest hop
-        n_ind, hop_time, rel_image = hop_times[0]
-        self.perform_hop(chromo_list[n_ind], hop_time, rel_image)
+        n_ind, hop_time, rel_img = hop_times[0]
+        if verbose:
+            print("\thop_times:")
+            print(*[f"\t{i} {j:.2e} {k}" for (i,j,k) in hop_times], sep="\n")
+            print(f"\tHopping to {n_ind}")
+        self.perform_hop(chromo_list[n_ind], hop_time, rel_img)
         return True
 
     def perform_hop(self, destination_chromo, hop_time, rel_image):
@@ -285,30 +290,23 @@ def update_molecule(atom_id, molecule_list, bonded_atoms):
 
 
 def run_single_kmc(
-        jobs_to_run,
+        jobs,
         KMC_directory,
-        param_dict,
         chromo_list,
         box,
-        cpu_rank,
-        seed,
-        send_end,
-        AA_morphdict=None,
-        use_avg_hop_rates=False,
-        record_history=True
+        temp,
+        carrier_kwargs={},
+        cpu_rank=None,
+        seed=None,
+        send_end=None,
         ):
-    global log_file
+    print(f"Found {len(jobs):d} jobs to run")
+    try:
+        use_avg_hop_rates = carrier_kwargs["use_avg_hop_rates"]
+    except KeyError:
+        use_avg_hop_rates = False
 
-    log_file = os.path.join(KMC_directory, f"KMC_log_{cpu_rank:02d}.log")
-    pickle_file = os.path.join(KMC_directory, f"kmc_{cpu_rank:02d}.pickle")
-    # Reset the log file
-    with open(log_file, "wb+") as log_file_handle:
-        pass
-    hf.write_to_file(log_file, [f"Found {len(jobs_to_run):d} jobs to run"])
-    # Set the affinities for this current process to make sure it's maximising
-    # available CPU usage
-    current_PID = os.getpid()
-    if use_avg_hop_rates is True:
+    if use_avg_hop_rates:
         # Chosen to split hopping by inter-intra molecular hops, so get
         # molecule data
         mol_id_dict = split_molecules(AA_morphdict, chromo_list)
@@ -316,155 +314,63 @@ def run_single_kmc(
         # the vals are the molids
     else:
         mol_id_dict = None
-    # Attempt to catch a kill signal to ensure that we save the pickle before
-    # termination
-    killer = termination_signal()
-    # Save the pickle as a list of `saveCarrier' instances that contain the
-    # bare minimum
-    save_data = initialise_save_data(len(chromo_list), seed)
-    if record_history is False:
-        save_data["hole_history"] = None
-        save_data["electron_history"] = None
     t0 = time.perf_counter()
-    save_time = time.perf_counter()
-    save_slot = "slot1"
-    try:
-        for job_number, [carrier_no, lifetime, ctype] in enumerate(jobs_to_run):
-            t1 = time.perf_counter()
-            # Find a random position to start the carrier in
-            while True:
-                i = np.random.randint(0, len(chromo_list) - 1)
-                i_chromo = chromo_list[i]
-                if (ctype == "electron") and (i_chromo.species != "acceptor"):
-                    continue
-                elif (ctype == "hole") and (i_chromo.species != "donor"):
-                    continue
-                break
-            # Create the carrier instance
-            i_carrier = Carrier(
-                i_chromo,
-                lifetime,
-                carrier_no,
-                box,
-                temp,
-                mol_id_dict=mol_id_dict,
-            )
-            terminate_simulation = False
-            while not terminate_simulation:
-                terminate_simulation = i_carrier.calculate_hop(chromo_list)
-                if killer.kill_sent is True:
-                    raise terminate(
-                        "Kill command sent, terminating KMC simulation..."
-                    )
-            # Now the carrier has finished hopping, let's calculate its vitals
-            i_carrier.displacement = i_carrier.calculate_displacement()
-            # Now the calculations are completed, create a barebones class
-            # containing the save data
-            importantData = [
-                "id",
-                "image",
-                "lifetime",
-                "current_time",
-                "n_hops",
-                "displacement",
-                "c_type",
-            ]
-            for name in importantData:
-                save_data[name].append(i_carrier.__dict__[name])
-            # Update the carrierHistoryMatrix
-            if record_history is True:
-                if i_carrier.c_type == "hole":
-                    save_data["hole_history"] += i_carrier.hole_history
-                elif i_carrier.c_type == "electron":
-                    save_data["electron_history"] += i_carrier.electron_history
-
-            # Then add in the initial and final positions
-            save_data["initial_position"].append(init_position)
-            save_data["final_position"].append(final_position)
-
-            t2 = time.perf_counter()
-            elapsed_time = float(t2) - float(t1)
-            if elapsed_time < 60:
-                time_units = "seconds."
-            elif elapsed_time < 3600:
-                elapsed_time /= 60.0
-                time_units = "minutes."
-            elif elapsed_time < 86400:
-                elapsed_time /= 3600.0
-                time_units = "hours."
-            else:
-                elapsed_time /= 86400.0
-                time_units = "days."
-
-            write_lines = [
-                    "{0:s} hopped {1:d} times, over {2:.2e} seconds, ".format(
-                        i_carrier.c_type.capitalize(),
-                        i_carrier.n_hops,
-                        i_carrier.current_time,
-                        ),
-                    f"into image {repr(i_carrier.image)}",
-                    ", for a displacement of {0:.2f}, in {1:.2f} ".format(
-                        i_carrier.displacement,
-                        elapsed_time
-                        ),
-                    f"wall-clock {time_units:s}"
-                    ]
-            hf.write_to_file(log_file, ["".join(write_lines)])
-            # Save the pickle file every hour
-            if (t2 - save_time) > 3600:
-                print(
-                    "Completed {0:d}/{1:d} jobs. Checkpoint at {2:3d}%".format(
-                        job_number,
-                        len(jobs_to_run),
-                        np.round((job_number + 1) / len(jobs_to_run) * 100)
-                    )
-                )
-                write_str= "Completed {0:d}/{1:d} jobs {2:3d}%".format(
-                        job_number,
-                        len(jobs_to_run),
-                        round((job_number + 1) / len(jobs_to_run) * 100)
-                        )
-                hf.write_to_file(log_file, [write_str])
-                save_pickle(save_data, pickle_file)
-                if save_slot.lower() == "slot1":
-                    save_slot = "slot2"
-                elif save_slot.lower() == "slot2":
-                    save_slot = "slot1"
-                save_time = time.perf_counter()
-    except Exception as error_message:
-        print(traceback.format_exc())
-        print("Saving the pickle file cleanly before termination...")
-        hf.write_to_file(log_file, [str(error_message)])
-        hf.write_to_file(
-            log_file, ["Saving the pickle file cleanly before termination..."]
+    carrier_list = []
+    for i_job, [carrier_no, lifetime, ctype] in enumerate(jobs):
+        print(f"starting job {i_job}")
+        t1 = time.perf_counter()
+        # Find a random position to start the carrier in
+        while True:
+            i = np.random.randint(0, len(chromo_list) - 1)
+            i_chromo = chromo_list[i]
+            if (ctype == "electron") and (i_chromo.species != "acceptor"):
+                continue
+            elif (ctype == "hole") and (i_chromo.species != "donor"):
+                continue
+            break
+        # Create the carrier instance
+        i_carrier = Carrier(
+            i_chromo,
+            lifetime,
+            carrier_no,
+            box,
+            temp,
+            len(chromo_list),
+            mol_id_dict=mol_id_dict,
         )
-        save_pickle(save_data, pickle_file)
-        print("Pickle saved! Exiting Python...")
-        exit()
+        continue_simulation = True
+        i=0
+        while continue_simulation:
+            continue_simulation = i_carrier.calculate_hop(chromo_list)
+            i += 1
+            if i > 100:
+                continue_simulation = False
+        # Now the carrier has finished hopping, let's calculate its vitals
+        i_carrier.update_displacement()
+
+        t2 = time.perf_counter()
+        elapsed_time = float(t2) - float(t1)
+        time_str = hf.time_units(elapsed_time)
+
+        print("\t{} hopped {} times, over {:.2e} seconds, ".format(
+            i_carrier.c_type,
+            i_carrier.n_hops,
+            i_carrier.current_time
+            )
+        )
+        print(f"\tinto image {i_carrier.image}")
+        print("\tfor a displacement of {:.2f} (took walltime {})".format(
+            i_carrier.displacement,
+            time_str
+            )
+        )
+        carrier_list.append(i_carrier)
     t3 = time.perf_counter()
     elapsed_time = float(t3) - float(t0)
-    if elapsed_time < 60:
-        time_units = "seconds."
-    elif elapsed_time < 3600:
-        elapsed_time /= 60.0
-        time_units = "minutes."
-    elif elapsed_time < 86400:
-        elapsed_time /= 3600.0
-        time_units = "hours."
-    else:
-        elapsed_time /= 86400.0
-        time_units = "days."
-    hf.write_to_file(
-        log_file,
-        ["All jobs completed in {0:.2f}{1:s}".format(elapsed_time, time_units)],
-    )
-    hf.write_to_file(
-            log_file,
-            ["Saving the pickle file cleanly before termination..."]
-            )
-    save_pickle(save_data, pickle_file)
-    hf.write_to_file(log_file, ["Exiting normally..."])
-    send_end.send(save_data)
+    time_str = hf.time_units(elapsed_time)
+    if send_end is not None:
+        send_end.send(save_data)
+    return carrier_list
 
 
 def get_jobslist(sim_times, n_holes=0, n_elec=0, nprocs=None, seed=None):
