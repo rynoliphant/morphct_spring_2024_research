afa7491a5f6354d4f65ef715969ae89b6597302e
Jenny Fothergill <jennyfothergill@u.boisestate.edu>
2021-03-03 13:16:53 -0700

    add basic starter test

---
+++ tests/test_imports.py
@@ -0,0 +1,8 @@
+def test_imports():
+    import morphct
+    from morphct import chromophores
+    from morphct import execute_qcc as eqcc
+    from morphct import helper_functions as hf
+    from morphct import mobility_kmc as kmc
+    from morphct import transfer_integrals as ti
+    from morphct.utils import kmc_analyze
