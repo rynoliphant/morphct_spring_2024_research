36da940b935eaba6a9ece1dc58246ab90a98b52f
Jenny Fothergill <jennyfothergill@u.boisestate.edu>
2021-03-03 14:54:01 -0700

    add conversion dicts to chromophores.py

--- morphct/chromophores.py
+++ morphct/chromophores.py
@@ -5,6 +5,7 @@ import os
 import sys
 from warnings import warn
 
+import ele
 import freud
 from openbabel import openbabel
 from openbabel import pybel
@@ -240,3 +241,93 @@ def set_neighbors_voronoi(chromo_list, snap, conversion_dict, d_cut=10):
                 )
             qcc_pairs.append(((i,j), qcc_input))
     return qcc_pairs
+
+
+conversion_dict = {
+    "S1": ele.element_from_symbol("S"),
+    "H1": ele.element_from_symbol("H"),
+    "C5": ele.element_from_symbol("C"),
+    "C1": ele.element_from_symbol("C"),
+    "C4": ele.element_from_symbol("C"),
+    "C6": ele.element_from_symbol("C"),
+    "C8": ele.element_from_symbol("C"),
+    "C9": ele.element_from_symbol("C"),
+    "C3": ele.element_from_symbol("C"),
+    "C7": ele.element_from_symbol("C"),
+    "C2": ele.element_from_symbol("C"),
+    "C10": ele.element_from_symbol("C"),
+}
+
+
+amber_dict = {
+    "c":  ele.element_from_symbol("C"),
+    "c1": ele.element_from_symbol("C"),
+    "c2": ele.element_from_symbol("C"),
+    "c3": ele.element_from_symbol("C"),
+    "ca": ele.element_from_symbol("C"),
+    "cp": ele.element_from_symbol("C"),
+    "cq": ele.element_from_symbol("C"),
+    "cc": ele.element_from_symbol("C"),
+    "cd": ele.element_from_symbol("C"),
+    "ce": ele.element_from_symbol("C"),
+    "cf": ele.element_from_symbol("C"),
+    "cg": ele.element_from_symbol("C"),
+    "ch": ele.element_from_symbol("C"),
+    "cx": ele.element_from_symbol("C"),
+    "cy": ele.element_from_symbol("C"),
+    "cu": ele.element_from_symbol("C"),
+    "cv": ele.element_from_symbol("C"),
+    "h1": ele.element_from_symbol("H"),
+    "h2": ele.element_from_symbol("H"),
+    "h3": ele.element_from_symbol("H"),
+    "h4": ele.element_from_symbol("H"),
+    "h5": ele.element_from_symbol("H"),
+    "ha": ele.element_from_symbol("H"),
+    "hc": ele.element_from_symbol("H"),
+    "hn": ele.element_from_symbol("H"),
+    "ho": ele.element_from_symbol("H"),
+    "hp": ele.element_from_symbol("H"),
+    "hs": ele.element_from_symbol("H"),
+    "hw": ele.element_from_symbol("H"),
+    "hx": ele.element_from_symbol("H"),
+    "f":  ele.element_from_symbol("F"),
+    "cl": ele.element_from_symbol("Cl"),
+    "br": ele.element_from_symbol("Br"),
+    "i":  ele.element_from_symbol("I"),
+    "n":  ele.element_from_symbol("N"),
+    "n1": ele.element_from_symbol("N"),
+    "n2": ele.element_from_symbol("N"),
+    "n3": ele.element_from_symbol("N"),
+    "n4": ele.element_from_symbol("N"),
+    "na": ele.element_from_symbol("N"),
+    "nb": ele.element_from_symbol("N"),
+    "nc": ele.element_from_symbol("N"),
+    "nd": ele.element_from_symbol("N"),
+    "ne": ele.element_from_symbol("N"),
+    "nf": ele.element_from_symbol("N"),
+    "nh": ele.element_from_symbol("N"),
+    "no": ele.element_from_symbol("N"),
+    "o":  ele.element_from_symbol("O"),
+    "oh": ele.element_from_symbol("O"),
+    "os": ele.element_from_symbol("O"),
+    "ow": ele.element_from_symbol("O"),
+    "p2": ele.element_from_symbol("P"),
+    "p3": ele.element_from_symbol("P"),
+    "p4": ele.element_from_symbol("P"),
+    "p5": ele.element_from_symbol("P"),
+    "pb": ele.element_from_symbol("P"),
+    "pc": ele.element_from_symbol("P"),
+    "pd": ele.element_from_symbol("P"),
+    "pe": ele.element_from_symbol("P"),
+    "pf": ele.element_from_symbol("P"),
+    "px": ele.element_from_symbol("P"),
+    "py": ele.element_from_symbol("P"),
+    "s":  ele.element_from_symbol("S"),
+    "s2": ele.element_from_symbol("S"),
+    "s4": ele.element_from_symbol("S"),
+    "s6": ele.element_from_symbol("S"),
+    "sh": ele.element_from_symbol("S"),
+    "ss": ele.element_from_symbol("S"),
+    "sx": ele.element_from_symbol("S"),
+    "sy": ele.element_from_symbol("S"),
+}
