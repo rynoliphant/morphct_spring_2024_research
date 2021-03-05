cd8f245cdf0477b337ac209b77bb377933e2c5a6
Jenny Fothergill <jennyfothergill@u.boisestate.edu>
2021-03-05 16:11:12 -0700

    up debug level

--- tests/test_execute_qcc.py
+++ tests/test_execute_qcc.py
@@ -8,5 +8,5 @@ class TestEQCC(BaseTest):
         from pysscf.semiempirical import MINDO3
 
         mol = pyscf.M(atom="H 0 0 0; F 0.9 0 0;")
-        mf = MINDO3(mol).run(verbose=4)
+        mf = MINDO3(mol).run(verbose=5)
 
