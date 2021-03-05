b90a719ddc52940fd7491cf43c5e0c8189f4a2ab
Jenny Fothergill <jennyfothergill@u.boisestate.edu>
2021-03-05 15:40:06 -0700

    add a basic pyscf test to try and debug the github actions issues

--- tests/test_execute_qcc.py
+++ tests/test_execute_qcc.py
@@ -3,6 +3,13 @@ from base_test import BaseTest
 
 class TestEQCC(BaseTest):
 
+    def test_pyscf(self):
+        import pyscf
+        from pysscf.semiempirical import MINDO3
+
+        mol = pyscf.M(atom="H 0 0 0; F 0.9 0 0;")
+        mf = MINDO3(mol).run(verbose=4)
+
     def test_singles_homolumo(self, p3ht_chromo_list):
         from morphct.execute_qcc import singles_homolumo
 
