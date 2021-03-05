6e2662a6884c872b620a80dd7d572ba021255b9a
Jenny Fothergill <jennyfothergill@u.boisestate.edu>
2021-03-05 16:15:06 -0700

    oops typo

--- tests/test_execute_qcc.py
+++ tests/test_execute_qcc.py
@@ -5,7 +5,7 @@ class TestEQCC(BaseTest):
 
     def test_pyscf(self):
         import pyscf
-        from pysscf.semiempirical import MINDO3
+        from pyscf.semiempirical import MINDO3
 
         mol = pyscf.M(atom="H 0 0 0; F 0.9 0 0;")
         mf = MINDO3(mol).run(verbose=5)
