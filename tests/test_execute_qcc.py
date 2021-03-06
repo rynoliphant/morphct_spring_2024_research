dd947d4dcd11c0e5d69df302671ed60a3c811d48
Jenny Fothergill <jennyfothergill@u.boisestate.edu>
2021-03-05 18:08:51 -0700

    chromo_list < nprocs in the test
    don't know why it worked locally

--- tests/test_execute_qcc.py
+++ tests/test_execute_qcc.py
@@ -13,12 +13,12 @@ class TestEQCC(BaseTest):
     def test_singles_homolumo(self, p3ht_chromo_list):
         from morphct.execute_qcc import singles_homolumo
 
-        chromo = p3ht_chromo_list[0]
-    #    data = singles_homolumo([chromo])
-    #    assert np.allclose(
-    #            data,
-    #            np.array([[-9.01337182, -8.5404688, 0.17193304, 0.86523495]])
-    #            )
+        chromos = p3ht_chromo_list[:2]
+        data = singles_homolumo(chromos)
+        assert np.allclose(
+                data[0],
+                np.array([[-9.01337182, -8.5404688, 0.17193304, 0.86523495]])
+                )
 
     #def test_dimer_homolumo(self, p3ht_qcc_pairs):
     #    from morphct.execute_qcc import dimer_homolumo
