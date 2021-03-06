18802a1bd48c49ecc4e9eb01dce575e6437be403
Jenny Fothergill <jennyfothergill@u.boisestate.edu>
2021-03-05 18:36:28 -0700

    revert back to just one chromo

--- tests/test_execute_qcc.py
+++ tests/test_execute_qcc.py
@@ -13,10 +13,10 @@ class TestEQCC(BaseTest):
     def test_singles_homolumo(self, p3ht_chromo_list):
         from morphct.execute_qcc import singles_homolumo
 
-        chromos = p3ht_chromo_list[:2]
-        data = singles_homolumo(chromos, nprocs=2)
+        chromo = p3ht_chromo_list[0]
+        data = singles_homolumo([chromo])
         assert np.allclose(
-                data[0],
+                data,
                 np.array([[-9.01337182, -8.5404688, 0.17193304, 0.86523495]])
                 )
 
