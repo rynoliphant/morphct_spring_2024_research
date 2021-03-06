78d0fd964fc81afff8a86a4e9cbacf058fc106e6
Jenny Fothergill <jennyfothergill@u.boisestate.edu>
2021-03-05 19:16:10 -0700

    add a test for repr and bad init

--- tests/test_chromophores.py
+++ tests/test_chromophores.py
@@ -5,19 +5,14 @@ from base_test import BaseTest
 
 class TestChromophores(BaseTest):
 
-    def test_simple(self):
-        from morphct.chromophores import Chromophore
+    def test_bad_init_chromophore(self, p3ht_snap):
+        from morphct.chromophores import Chromophore, conversion_dict
         with pytest.raises(TypeError):
             Chromophore()
 
-
-    def test_chromos_from_smiles(self, p3ht_snap):
-        from morphct.chromophores import get_chromo_ids_smiles, conversion_dict
-
-        p3ht_smarts = "[#6]1[#6][#16][#6][#6]1CCCCCC"
-        aaids = get_chromo_ids_smiles(p3ht_snap, p3ht_smarts, conversion_dict)
-        assert len(aaids) == 29
-        assert np.all([len(i)  == 11 for i in aaids])
+        atom_ids = np.array([1, 0, 4, 3, 2, 5, 6, 7, 8, 9, 10])
+        with pytest.raises(TypeError):
+            Chromophore(0, p3ht_snap, atom_ids, "bad_species", conversion_dict)
 
     def test_init_chromophore(self, p3ht_snap):
         from morphct.chromophores import Chromophore, conversion_dict
@@ -51,6 +46,14 @@ class TestChromophores(BaseTest):
                 )
         assert chromo.vrh_delocalization == 2e-10
 
+    def test_chromos_from_smiles(self, p3ht_snap):
+        from morphct.chromophores import get_chromo_ids_smiles, conversion_dict
+
+        p3ht_smarts = "[#6]1[#6][#16][#6][#6]1CCCCCC"
+        aaids = get_chromo_ids_smiles(p3ht_snap, p3ht_smarts, conversion_dict)
+        assert len(aaids) == 29
+        assert np.all([len(i)  == 11 for i in aaids])
+
     def test_set_neighbors_voronoi(self, p3ht_snap, p3ht_chromo_list):
         from morphct.chromophores import set_neighbors_voronoi, conversion_dict
 
@@ -71,3 +74,10 @@ class TestChromophores(BaseTest):
             qcc_input.split(";")[0] ==
             'C -3.7486698030653614 -0.26928230395247255 -1.6724275287954669'
         )
+
+    def test_repr(self, p3ht_chromo_list):
+        chromo = p3ht_chromo_list[0]
+        assert(
+                repr(chromo) ==
+                'Chromophore 0 (donor): 11 atoms at 8.532 -27.966 -35.075'
+                )
