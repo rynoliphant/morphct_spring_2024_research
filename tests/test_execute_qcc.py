37a969a28ca8d497c7cd2d64046d7b0478194751
Jenny Fothergill <jennyfothergill@u.boisestate.edu>
2021-03-05 18:39:02 -0700

    add back in failing tests (again)
    * hopefully they aren't failing now!

--- tests/test_execute_qcc.py
+++ tests/test_execute_qcc.py
@@ -20,32 +20,32 @@ class TestEQCC(BaseTest):
                 np.array([[-9.01337182, -8.5404688, 0.17193304, 0.86523495]])
                 )
 
-    #def test_dimer_homolumo(self, p3ht_qcc_pairs):
-    #    from morphct.execute_qcc import dimer_homolumo
+    def test_dimer_homolumo(self, p3ht_qcc_pairs):
+        from morphct.execute_qcc import dimer_homolumo
 
-    #    qcc_pair = p3ht_qcc_pairs[0]
-    #    pair, energies = dimer_homolumo([qcc_pair])[0]
+        qcc_pair = p3ht_qcc_pairs[0]
+        pair, energies = dimer_homolumo([qcc_pair])[0]
 
-    #    assert pair == (0,1)
-    #    assert np.allclose(
-    #            energies,
-    #            np.array([-8.70917208, -8.21756382, -0.26434042, 0.35184321])
-    #            )
+        assert pair == (0,1)
+        assert np.allclose(
+                energies,
+                np.array([-8.70917208, -8.21756382, -0.26434042, 0.35184321])
+                )
 
-    #def test_set_energyvalues(
-    #    self, p3ht_chromo_list_neighbors, p3ht_sfilename, p3ht_dfilename
-    #    ):
-    #    from morphct.execute_qcc import set_energyvalues
+    def test_set_energyvalues(
+        self, p3ht_chromo_list_neighbors, p3ht_sfilename, p3ht_dfilename
+        ):
+        from morphct.execute_qcc import set_energyvalues
 
-    #    set_energyvalues(
-    #            p3ht_chromo_list_neighbors, p3ht_sfilename, p3ht_dfilename
-    #            )
+        set_energyvalues(
+                p3ht_chromo_list_neighbors, p3ht_sfilename, p3ht_dfilename
+                )
 
-    #    chromo = p3ht_chromo_list_neighbors[0]
+        chromo = p3ht_chromo_list_neighbors[0]
 
-    #    assert chromo.homo_1 == -9.013371824203956
-    #    assert chromo.homo == -8.54046879758057
-    #    assert chromo.lumo == 0.1719330393321203
-    #    assert chromo.lumo_1 == 0.8652349542720108
-    #    assert chromo.neighbors_delta_e[0] == -0.016112646653095197
-    #    assert chromo.neighbors_ti[0] == 0.2456720694088973
+        assert chromo.homo_1 == -9.013371824203956
+        assert chromo.homo == -8.54046879758057
+        assert chromo.lumo == 0.1719330393321203
+        assert chromo.lumo_1 == 0.8652349542720108
+        assert chromo.neighbors_delta_e[0] == -0.016112646653095197
+        assert chromo.neighbors_ti[0] == 0.2456720694088973
