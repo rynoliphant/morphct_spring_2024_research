fe6a86b23fa5a66f0865f04999edcb6b585ed025
Jenny Fothergill <jennyfothergill@u.boisestate.edu>
2021-03-22 18:10:18 -0600

    oops forgot i deleted utils

--- morphct/__init__.py
+++ morphct/__init__.py
@@ -2,7 +2,7 @@ from morphct.chromophores import Chromophore
 from morphct.mobility_kmc import Carrier
 from morphct.__version__ import __version__
 from morphct import (chromophores, execute_qcc, helper_functions, kmc_analyze,
-                mobility_kmc, transfer_integrals, utils)
+                mobility_kmc, transfer_integrals)
 
 __all__ = [
         "__version__",
@@ -14,5 +14,4 @@ __all__ = [
         "kmc_analyze",
         "mobility_kmc",
         "transfer_integrals",
-        "utils"
         ]
