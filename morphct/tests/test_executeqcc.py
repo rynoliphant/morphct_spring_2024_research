56af3dde64cca862f2de0e81c28b3c6b6debe948
Jenny Fothergill <jennyfothergill@u.boisestate.edu>
2021-07-02 10:34:48 -0600

    add charge to asset and chromolist to arg

--- morphct/tests/test_executeqcc.py
+++ morphct/tests/test_executeqcc.py
@@ -21,11 +21,11 @@ class TestEQCC(BaseTest):
             data, np.array([[-9.01337182, -8.5404688, 0.17193304, 0.86523495]])
         )
 
-    def test_dimer_homolumo(self, p3ht_qcc_pairs):
+    def test_dimer_homolumo(self, p3ht_qcc_pairs, p3ht_chromo_list):
         from morphct.execute_qcc import dimer_homolumo
 
         qcc_pair = p3ht_qcc_pairs[0]
-        pair, energies = dimer_homolumo([qcc_pair])[0]
+        pair, energies = dimer_homolumo([qcc_pair], p3ht_chromo_list)[0]
 
         assert pair == (0, 1)
         assert np.allclose(
